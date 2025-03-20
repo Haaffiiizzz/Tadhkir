c552f8a19d958969c841135feb2901476658d1db
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-03-20 01:55:33 -0500

    Now goes to getname if not already name, and goes to getlocation is already name and not location

--- tadhkir/app/(tabs)/index.tsx
+++ tadhkir/app/(tabs)/index.tsx
@@ -7,37 +7,48 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const userSetup = () => {
     const router = useRouter();
     const [firstName, setFirstName] = useState<string | null>(null);
+    const [latitude, setLatitude] = useState<string | null>(null);
+
     const getName = async () => {
         const storedFirstName = await AsyncStorage.getItem('userName');
         setFirstName(storedFirstName);
     };
 
+    const getLatitude = async () => {
+        const storedLatitude = await AsyncStorage.getItem('latitude');
+        setLatitude(storedLatitude);
+    };
+
     useFocusEffect(
         React.useCallback(() => {
             getName();
+            getLatitude();
         }, [])
     );
-            
+
     useEffect(() => {
         if (!firstName) {
             const timer = setTimeout(() => {
                 router.push('/others/userName');
             }, 0);
             return () => clearTimeout(timer);
+        } else if (firstName && !latitude) {
+            const timer = setTimeout(() => {
+                router.push('/others/userLocation');
+            }, 0);
+            return () => clearTimeout(timer);
         }
-    }, [firstName]); 
+    }, [firstName, latitude]);
 
     return (
         <View style={styles.container}>
-
-            <>
-                <Text>Welcome back {firstName}</Text>
-
-                <Button title="Clear All Data" 
-                    onPress= {()=> {AsyncStorage.clear()}}     
-                />
-            </>
-        
+            <Text>Welcome back {firstName}</Text>
+            <Button
+                title="Clear All Data"
+                onPress={() => {
+                    AsyncStorage.clear();
+                }}
+            />
         </View>
     );
 };
