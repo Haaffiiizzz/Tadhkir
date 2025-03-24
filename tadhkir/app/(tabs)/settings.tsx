8ac7062dfa654338e0e72088fd2b2789722e7865
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-03-23 23:45:23 -0500

    Add time format toggle to settings page

--- tadhkir/app/(tabs)/settings.tsx
+++ tadhkir/app/(tabs)/settings.tsx
@@ -1,6 +1,8 @@
 import { View, Button, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Updates from 'expo-updates';
+import React, {useState, useEffect} from 'react';
+import { Switch } from 'react-native-switch';
 
 
 
@@ -12,15 +14,47 @@ export default function Settings() {
           console.error(e);
         }
       };
-      
+
+    const [is24Hour, setIs24Hour] = useState(false);
+
+    useEffect(() => {
+        
+        const loadTimeFormat = async () => {
+            const storedFormat = await AsyncStorage.getItem('timeformat');
+            if (storedFormat === "24h") {
+                setIs24Hour(true);
+            } else {
+                setIs24Hour(false);
+            }
+        };
+
+        loadTimeFormat();
+    }, []);
+
+    const changeTimeFormat = async () => {
+        const newFormat = is24Hour ? "12h" : "24h";
+        await AsyncStorage.setItem('timeformat', newFormat);
+        setIs24Hour(!is24Hour);
+    };
+
     return (
-        <View style={styles.container}>  
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Button
                 title="Clear All Data"
                 onPress={() => {
-                    AsyncStorage.clear(),
+                    AsyncStorage.clear();
                     restartApp();
                 }}
+            />
+
+            <Switch
+                trackColor={{false: '#767577', true: '#81b0ff'}}
+                thumbColor={is24Hour ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={changeTimeFormat}
+                value={is24Hour}
+                activeText={'12h'}
+                inActiveText={'24h'}
             />
         </View>
     );
