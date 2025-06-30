2212731453a84a6e7a5cc08a67e53a1815bb3dd8
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-06-30 00:18:18 -0500

    Manually picking location works. Only fixed locations for now. Need to be able to change location in settings without losing data.

--- tadhkir/app/GetUserLocation.tsx
+++ tadhkir/app/GetUserLocation.tsx
@@ -9,21 +9,22 @@ import { Dropdown } from 'react-native-element-dropdown';
 const userSetup = () => {
     const router = useRouter();
     const [locationReady, setLocationReady] = useState(false)
+    const [value, setValue] = useState(null);
 
     const predefinedLocations = [
-        { name: "Cairo, Egypt", value: { lat: 30.0444, lon: 31.2357 } },
-        { name: "Riyadh, Saudi Arabia", value: { lat: 24.7136, lon: 46.6753 } },
-        { name: "Istanbul, Turkey", value: { lat: 41.0082, lon: 28.9784 } },
-        { name: "Jakarta, Indonesia", value: { lat: -6.2088, lon: 106.8456 } },
-        { name: "London, UK", value: { lat: 51.5074, lon: -0.1278 } },
-        { name: "New York, USA", value: { lat: 40.7128, lon: -74.0060 } },
-        { name: "Karachi, Pakistan", value: { lat: 24.8607, lon: 67.0011 } },
-        { name: "Lagos, Nigeria", value: { lat: 6.5244, lon: 3.3792 } },
-        { name: "Kuala Lumpur, Malaysia", value: { lat: 3.1390, lon: 101.6869 } },
-        { name: "Toronto, Canada", value: { lat: 43.6532, lon: -79.3832 } },
-        { name: "Winnipeg, Canada", value: { lat: 49.8951, lon: -97.1384 } },
-        { name: "Edmonton, Canada", value: { lat: 53.5461, lon: -113.4938 } },
-        { name: "Abuja, Nigeria", value: { lat: 9.0765, lon: 7.3986 } },
+        { label: "Cairo, Egypt", value: { latitude: 30.0444, longitude: 31.2357 } },
+        { label: "Riyadh, Saudi Arabia", value: { latitude: 24.7136, longitude: 46.6753 } },
+        { label: "Istanbul, Turkey", value: { latitude: 41.0082, longitude: 28.9784 } },
+        { label: "Jakarta, Indonesia", value: { latitude: -6.2088, longitude: 106.8456 } },
+        { label: "London, UK", value: { latitude: 51.5074, longitude: -0.1278 } },
+        { label: "New York, USA", value: { latitude: 40.7128, longitude: -74.0060 } },
+        { label: "Karachi, Pakistan", value: { latitude: 24.8607, longitude: 67.0011 } },
+        { label: "Lagos, Nigeria", value: { latitude: 6.5244, longitude: 3.3792 } },
+        { label: "Kuala Lumpur, Malaysia", value: { latitude: 3.1390, longitude: 101.6869 } },
+        { label: "Toronto, Canada", value: { latitude: 43.6532, longitude: -79.3832 } },
+        { label: "Winnipeg, Canada", value: { latitude: 49.8951, longitude: -97.1384 } },
+        { label: "Edmonton, Canada", value: { latitude: 53.5461, longitude: -113.4938 } },
+        { label: "Abuja, Nigeria", value: { latitude: 9.0765, longitude: 7.3986 } },
     ];
 
 
@@ -51,6 +52,12 @@ const userSetup = () => {
         }
     };
     
+    const getPrayerFunction = async (latitude: number, longitude: number) => {
+        if(latitude && longitude){
+            await prayerStorageMain(latitude.toString(), longitude.toString());
+            setLocationReady(true);
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -62,16 +69,27 @@ const userSetup = () => {
                         const locationData = await requestLocation();
                         if (locationData) {
                             const [latitude, longitude] = locationData;
-                            await prayerStorageMain(latitude.toString(), longitude.toString());
-                            setLocationReady(true);
+                            await getPrayerFunction(latitude, longitude);
                         }
                     }}
                 />
                 <Text>Or select manually below</Text>
-                <Dropdown
+                <Dropdown //use on  confirm selection 
                     style={styles.dropdown}
                     data={predefinedLocations}
-                    onChange={() => {}}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    maxHeight={300}
+                    onChange={async (item) => {
+                        const latitude = item.value.latitude
+                        const longitude = item.value.longitude
+                        setValue(item.label)
+                        await getPrayerFunction(latitude, longitude)
+                    }}
+                    placeholder={value ? value : "Please make a selection!"}
+                    labelField="label"
+                    valueField="value"
+                    value= {value}
                 
                 />
 
@@ -106,6 +124,12 @@ const styles = StyleSheet.create({
         width: 150,
         borderBottomColor: 'gray',
         borderBottomWidth: 0.5,
+    },
+    placeholderStyle: {
+    fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
     },
 });
 export default userSetup;
