794adb1a4fa3cd30bc307271b751f15bdfdf31a3
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-07-05 23:42:08 -0500

    Working on user auth

--- tadhkir/app/(tabs)/SignIn.tsx
+++ tadhkir/app/(tabs)/SignIn.tsx
@@ -1,7 +1,29 @@
-import {Text} from "react-native"
+import {Text, TextInput, Button,View} from "react-native"
+import React, { useEffect, useState } from 'react';
 const SignIn = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-        <Text>Hello</Text>
+        <View>
+        <TextInput style={styles.textInput}
+            onChangeText={setEmail}
+            value={email}
+            placeholder='Enter username or email address'
+        />
+
+        <TextInput style={styles.textInput}
+            onChangeText={setPassword}
+            value={password}
+            placeholder='Enter password.'
+        />
+        
+        <Button
+            title="Submit" 
+            onPress={ async () => { 
+             console.log(email, password)
+            }} 
+        />
+        </View>
     )
 }
 export default SignIn;
